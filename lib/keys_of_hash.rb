c4270f25dbc3b2f31e6f3bbda092c686c75df8da
Matt Doering <mattdoering86@gmail.com>
2018-11-19 01:13:17 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,11 +3,7 @@ class Hash
   def keys_of(*arguments)
     array = []
   arguments.each do |arg|
-    if arguments == arg
-      binding.pry
-      array << arg
-    end
+  binding.pry
   end
-  array
 end
 end
